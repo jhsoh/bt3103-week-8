9cf54f5fab1b81b7ff526ddb86c03d65141ed499
jhsoh <sohjh98@gmail.com>
2021-03-15 16:44:38 +0800

    project file added

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>BT3103 week 9</header>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
